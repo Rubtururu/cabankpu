f4a71c171384af6695b44f177895edc118db7541
Rubtururu <59855904+Rubtururu@users.noreply.github.com>
2024-05-18 17:01:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,293 +1,51 @@
-document.addEventListener('DOMContentLoaded', async () => {
-  if (window.ethereum) {
-    window.web3 = new Web3(window.ethereum);
-    await window.ethereum.enable();
+document.addEventListener("DOMContentLoaded", async () => {
+    if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        await window.ethereum.enable();
 
-    const contractAddress = '0x06A0764948FcBaAC400F4681C2707c680444F29d';
-    const contractABI = [
-      {
-        inputs: [],
-        stateMutability: 'nonpayable',
-        type: 'constructor',
-      },
-      {
-        inputs: [
-          {
-            indexed: true,
-            internalType: 'address',
-            name: 'user',
-            type: 'address',
-          },
-          {
-            indexed: false,
-            internalType: 'uint256',
-            name: 'amount',
-            type: 'uint256',
-},
-        ],
-        name: 'Deposit',
-        type: 'event',
-      },
-      {
-        inputs: [
-          {
-            indexed: true,
-            internalType: 'address',
-            name: 'user',
-            type: 'address',
-          },
-          {
-            indexed: false,
-            internalType: 'uint256',
-            name: 'amount',
-            type: 'uint256',
-          },
-        ],
-        name: 'Withdraw',
-        type: 'event',
-      },
-      {
-        inputs: [
-          {
-            indexed: true,
-            internalType: 'address',
-            name: 'user',
-            type: 'address',
-          },
-          {
-            indexed: false,
-            internalType: 'uint256',
-            name: 'amount',
-            type: 'uint256',
-          },
-        ],
-        name: 'ClaimDividends',
-        type: 'event',
-      },
-      {
-        inputs: [],
-        name: 'deposit',
-        outputs: [],
-        stateMutability: 'payable',
-        type: 'function',
-      },
-      {
-        inputs: [
-          {
-            internalType: 'uint256',
-            name: 'amount',
-            type: 'uint256',
-          },
-        ],
-        name: 'withdraw',
-        outputs: [],
-        stateMutability: 'nonpayable',
-        type: 'function',
-      },
-      {
-        inputs: [],
-        name: 'claimDividends',
-        outputs: [],
-        stateMutability: 'nonpayable',
-        type: 'function',
-      },
-      {
-        inputs: [],
-        name: 'getContractBalance',
-        outputs: [
-          {
-            internalType: 'uint256',
-            name: '',
-            type: 'uint256',
-          },
-        ],
-        stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        inputs: [],
-        name: 'getUserDailyDividends',
-        outputs: [
-          {
-            internalType: 'uint256',
-            name: '',
-            type: 'uint256',
-          },
-        ],
-        stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        inputs: [],
-        name: 'lastDividendsPaymentTime',
-        outputs: [
-          {
-            internalType: 'uint256',
-            name: '',
-            type: 'uint256',
-          },
-        ],
-        stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        inputs: [],
-        name: 'totalDeposits',
-        outputs: [
-          {
-            internalType: 'uint256',
-            name: '',
-            type: 'uint256',
-          },
-        ],
-        stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        inputs: [],
-        name: 'totalTreasuryPool',
-        outputs: [
-          {
-            internalType: 'uint256',
-            name: '',
-            type: 'uint256',
-          },
-        ],
-        stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        inputs: [],
-        name: 'totalDividendsPool',
-        outputs: [
-          {
-            internalType: 'uint256',
-            name: '',
-            type: 'uint256',
-          },
-        ],
-        stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        inputs: [
-          {
-            internalType: 'address',
-            name: '',
-            type: 'address',
-          },
-        ],
-        name: 'userDeposits',
-        outputs: [
-          {
-            internalType: 'uint256',
-            name: '',
-            type: 'uint256',
-          },
-        ],
-        stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        inputs: [
-          {
-            internalType: 'address',
-            name: '',
-            type: 'address',
-          },
-        ],
-        name: 'userWithdrawals',
-       outputs: [
-          {
-            internalType: 'uint256',
-            name: '',
-            type: 'uint256',
-          },
-        ],
-        stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        inputs: [
-          {
-            internalType: 'address',
-            name: '',
-            type: 'address',
-          },
-        ],
-        name: 'userDividendsClaimed',
-        outputs: [
-          {
-            internalType: 'uint256',
-            name: '',
-            type: 'uint256',
-          },
-        ],
-        stateMutability: 'view',
-        type: 'function',
-      },
-    ];
+        const contractAddress = '0xfBA4051e9E284Ba09e98bA43450a057C78A596ed';
+        const contractABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"ceo","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"claimDividends","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"deposit","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"getStats","outputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"userAddress","type":"address"}],"name":"getUserStats","outputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"lastDividendsPaymentTime","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalDeposits","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalDividendsPool","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalTreasuryPool","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"userAddresses","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"users","outputs":[{"internalType":"uint256","name":"deposits","type":"uint256"},{"internalType":"uint256","name":"withdrawals","type":"uint256"},{"internalType":"uint256","name":"totalDividends","type":"uint256"},{"internalType":"uint256","name":"lastDividends","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"stateMutability":"payable","type":"receive"}];
 
-    const contract = new web3.eth.Contract(contractABI, contractAddress);
+        const contract = new web3.eth.Contract(contractABI, contractAddress);
 
-    const accounts = await web3.eth.getAccounts();
-    const userAccount = accounts[0];
+        const userAddress = (await web3.eth.getAccounts())[0];
+        document.getElementById("user-address").innerText = userAddress;
 
-    updateStats(); // Update initial stats
+        async function updateStats() {
+            const [totalDeposits, totalTreasuryPool, totalDividendsPool, lastDividendsPaymentTime] = await contract.methods.getStats().call();
+            document.getElementById("total-deposits").innerText = web3.utils.fromWei(totalDeposits, 'ether');
+            document.getElementById("total-treasury-pool").innerText = web3.utils.fromWei(totalTreasuryPool, 'ether');
+            document.getElementById("total-dividends-pool").innerText = web3.utils.fromWei(totalDividendsPool, 'ether');
+            document.getElementById("last-dividends-payment-time").innerText = new Date(lastDividendsPaymentTime * 1000).toLocaleString();
 
-    // Event listeners for deposit, withdraw, and claim dividends buttons
-    document.getElementById('deposit-form').addEventListener('submit', async (e) => {
-      e.preventDefault();
-      const amount = document.getElementById('deposit-amount').value;
-      await contract.methods.deposit().send({ from: userAccount, value: web3.utils.toWei(amount, 'ether') });
-      updateStats();
-      document.getElementById('deposit-amount').value = ''; // Clear field after deposit
-    });
+            const [userDeposits, userWithdrawals, userTotalDividends, userLastDividends] = await contract.methods.getUserStats(userAddress).call();
+            document.getElementById("user-deposits").innerText = web3.utils.fromWei(userDeposits, 'ether');
+            document.getElementById("user-withdrawals").innerText = web3.utils.fromWei(userWithdrawals, 'ether');
+            document.getElementById("user-total-dividends").innerText = web3.utils.fromWei(userTotalDividends, 'ether');
+            document.getElementById("user-dividends-today").innerText = web3.utils.fromWei(userLastDividends, 'ether');
+        }
 
-    document.getElementById('withdraw-form').addEventListener('submit', async (e) => {
-      e.preventDefault();
-      const amount = document.getElementById('withdraw-amount').value;
-      await contract.methods.withdraw(web3.utils.toWei(amount, 'ether')).send({ from: userAccount });
-      updateStats();
-      document.getElementById('withdraw-amount').value = ''; // Clear field after withdraw
-    });
+        document.getElementById("deposit-form").addEventListener("submit", async (event) => {
+            event.preventDefault();
+            const depositAmount = document.getElementById("deposit-amount").value;
+            await contract.methods.deposit().send({ from: userAddress, value: web3.utils.toWei(depositAmount, 'ether') });
+            await updateStats();
+        });
 
-    document.getElementById('claim-dividends').addEventListener('click', async () => {
-      await contract.methods.claimDividends().send({ from: userAccount });
-      updateStats();
-    });
+        document.getElementById("withdraw-form").addEventListener("submit", async (event) => {
+            event.preventDefault();
+            const withdrawAmount = document.getElementById("withdraw-amount").value;
+            await contract.methods.withdraw(web3.utils.toWei(withdrawAmount, 'ether')).send({ from: userAddress });
+            await updateStats();
+        });
 
-    async function updateStats() {
-      // Get contract stats
-      const totalDeposits = await contract.methods.totalDeposits().call();
-      const totalTreasuryPool = await contract.methods.totalTreasuryPool().call();
-      const totalDividendsPool = await contract.methods.totalDividendsPool().call();
-      const lastDividendsPaymentTime = await contract.methods.lastDividendsPaymentTime().call();
-      const contractBalance = await contract.methods.getContractBalance().call();
+        document.getElementById("claim-dividends").addEventListener("click", async () => {
+            await contract.methods.claimDividends().send({ from: userAddress });
+            await updateStats();
+        });
 
-      // Get user stats
-      const userDeposits = await contract.methods.userDeposits(userAccount).call();
-      const userWithdrawals = await contract.methods.userWithdrawals(userAccount).call();
-      const userDividendsToday = await contract.methods.getUserDailyDividends(userAccount).call();
-      const userCurrentDeposit = parseInt(userDeposits) - parseInt(userWithdrawals);
-      const userTotalWithdrawals = userWithdrawals;
-      const userTotalDividends = await contract.methods.userDividendsClaimed(userAccount).call();
-
-      // Update HTML elements with stats
-      document.getElementById('user-address').innerText = userAccount;
-      document.getElementById('total-deposits').innerText = web3.utils.fromWei(totalDeposits, 'ether');
-      document.getElementById('total-treasury-pool').innerText = web3.utils.fromWei(totalTreasuryPool, 'ether');
-      document.getElementById('total-dividends-pool').innerText = web3.utils.fromWei(totalDividendsPool, 'ether');
-      document.getElementById('last-dividends-payment-time').innerText = new Date(lastDividendsPaymentTime * 1000).toLocaleString();
-      document.getElementById('user-deposits').innerText = web3.utils.fromWei(userDeposits, 'ether');
-      document.getElementById('user-withdrawals').innerText = web3.utils.fromWei(userWithdrawals, 'ether');
-      document.getElementById('contract-balance').innerText = web3.utils.fromWei(contractBalance, 'ether');
-      document.getElementById('user-dividends-today').innerText = web3.utils.fromWei(userDividendsToday, 'ether');
-      document.getElementById('user-current-deposit').innerText = web3.utils.fromWei(userCurrentDeposit.toString(), 'ether');
-      document.getElementById('user-total-withdrawals').innerText = web3.utils.fromWei(userTotalWithdrawals, 'ether');
-      document.getElementById('user-total-dividends').innerText = web3.utils.fromWei(userTotalDividends, 'ether');
+        await updateStats();
+    } else {
+        alert('Please install MetaMask to use this application.');
     }
-  } else {
-    alert('Please install MetaMask to use this application.');
-  }
 });
